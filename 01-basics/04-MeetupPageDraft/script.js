fb41f0f64cdb644e0bc78535620fe280d11f78a9
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-04-05 21:08:12 +0300

    Added task Meetup Page Draft

---
+++ 01-basics/04-MeetupPageDraft/script.js
@@ -0,0 +1,47 @@
+import Vue from './vendor/vue.esm.browser.js';
+
+/** URL адрес API */
+const API_URL = 'https://course-vue.javascript.ru/api';
+
+/** ID митапа для примера; используйте его при получении митапа */
+const MEETUP_ID = 6;
+
+/**
+ * Возвращает ссылку на изображение по идентификатору, например, изображение митапа
+ * @param imageId {number} - идентификатор изображения
+ * @return {string} - ссылка на изображение
+ */
+function getImageUrlByImageId(imageId) {
+  return `${API_URL}/images/${imageId}`;
+}
+
+/**
+ * Функция, возвращающая словарь заголовков по умолчанию для всех типов пунктов программы
+ */
+const getAgendaItemDefaultTitles = () => ({
+  registration: 'Регистрация',
+  opening: 'Открытие',
+  break: 'Перерыв',
+  coffee: 'Coffee Break',
+  closing: 'Закрытие',
+  afterparty: 'Afterparty',
+  talk: 'Доклад',
+  other: 'Другое',
+});
+
+/**
+ * Функция, возвращая словарь иконок для для всех типов пунктов программы.
+ * Соответствует имени иконок в директории /assets/icons
+ */
+const getAgendaItemIcons = () => ({
+  registration: 'key',
+  opening: 'cal-sm',
+  talk: 'tv',
+  break: 'clock',
+  coffee: 'coffee',
+  closing: 'key',
+  afterparty: 'cal-sm',
+  other: 'cal-sm',
+});
+
+new Vue();
