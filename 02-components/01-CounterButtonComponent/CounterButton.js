6f1d65aa50c8bf11e018574e637950cec85daf55
Andrey Sekretarev <andrey.sekretarev@simple-solution.biz>
2021-04-10 19:05:47 +0300

    added solution for task 1 (components)

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -2,11 +2,27 @@ export default {
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр
+  props: {
+    inputCounter: Number,
+  },
 
   // Компонент должен иметь модель
+  data() {
+    return {
+      localCounter: 0,
+    };
+  },
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
+  methods: {
+    doIncrement() {
+      this.$emit('increment', ++this.localCounter);
+    },
+  },
 
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: `<button
+              type="button"
+              @click="doIncrement"
+             >{{ localCounter }}</button>`,
 };
