75a484f7d93ca4e996cc98e3a14b817b3a408057
Andrey Sekretarev <andrey.sekretarev@simple-solution.biz>
2021-04-11 11:23:28 +0300

    added solution for task MeetupInfo (look my comments)

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -1,19 +1,48 @@
 export default {
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+    },
+
+    place: {
+      type: String,
+      required: true,
+    },
+
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+
+  computed: {
+    humanDate() {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' };
+      // return this.date.toLocaleDateString('ru-RU', options); //человеческий формат авто тест не принимает
+      return this.date.toLocaleDateString('en-EN', options); // пришлось вернуть en-EN формат
+    },
+
+    robotDate() {
+      return this.date.toISOString().split('T')[0];
+    },
+  },
+
   template: `
     <ul class="info-list">
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="robotDate">{{ humanDate }}</time>
       </li>
     </ul>`,
 };
