aaf67bd62da897db75059a2ea48bd0aaf013f932
warma2d <warma2d@ya.ru>
2021-06-16 19:01:27 +0300

    try solve DateInput

--- 06-wrappers/05-DateInput/components/DateInput.vue
+++ 06-wrappers/05-DateInput/components/DateInput.vue
@@ -1,5 +1,9 @@
 <template>
-  <app-input />
+  <app-input
+    v-bind="localAttrs"
+    v-on="localListeners"
+    @change="changeMethod"
+  ><slot></slot></app-input>
 </template>
 
 <script>
@@ -7,7 +11,56 @@ import AppInput from './AppInput';
 
 export default {
   name: 'DateInput',
+  inheritAttrs: false,
 
   components: { AppInput },
+
+  model: {
+    prop: 'value',
+    event: 'change',
+  },
+
+  props: {
+    type: {
+      type: String,
+      default: 'date',
+    },
+
+    value: {
+      type: [Number, Date, String],
+    }
+  },
+
+  computed: {
+    localAttrs() {
+      let attrs = { ...this.$attrs }
+      attrs['type'] = this.type
+
+      return attrs
+    },
+
+    localListeners() {
+      let listeners = { ...this.$listeners }
+
+      for(let key in listeners) {
+        if (key === 'input' || key === 'change') {
+          delete listeners[key]
+        }
+      }
+      return listeners
+    },
+
+  },
+
+  mounted() {
+
+  },
+
+  methods: {
+    changeMethod(event) {
+      this.$emit('change', event.target.value)
+    },
+  },
+
 };
 </script>
